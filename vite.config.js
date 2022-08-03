fb877df9989a55f75d716fd0b8783999b6c4050a
Simon Gardette <sg@simon-gardette.fr>
2022-08-03 17:38:49 +0200

    fix directory

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,4 @@ import { VitePWA } from "vite-plugin-pwa";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), VitePWA({ registerType: "autoUpdate" })],
-  publicDir: "assets",
 });
